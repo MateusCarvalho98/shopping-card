880d4ef13b1a819fcd30b3cb453a31d0a155c489
Mateus Carvalho <mateuscarvalho_pbi@hotmail.com>
2023-08-06 20:09:33 -0300

    Finish the project

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './ProductCard.css';
 import { BsFillCartPlusFill } from 'react-icons/bs';
 import propTypes from 'prop-types';
 import formatCurrency from '../../utils/formatCurrency';
+import AppContext from '../../context/AppContext';
 
 
 function ProductCard({ data }) {
 
   const { title, thumbnail, price } = data;
+
+  const { cartItems, setCartItems } = useContext(AppContext);
+  /* obs: esses tres pontos abaixo "..." passa todos os items do array*/
+  const handleAddCart = () => setCartItems([ ...cartItems, data]);
 
   return (
     <section className="product-card">
@@ -21,7 +26,11 @@ function ProductCard({ data }) {
         <h2 className="card__title">{title}</h2>
         
       </div>
-      <button type="button" className="button__add-cart">
+      <button 
+        type="button" 
+        className="button__add-cart"
+        onClick={ handleAddCart }  
+      >
         <BsFillCartPlusFill />
       </button>
     </section>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import './SearchBar.css';
+import fetchProducts from '../../api/fetchProducts';
+import appContext from '../../context/AppContext';
 
 function SearchBar() {
 
+  const { setProducts, setLoading } = useContext(appContext);
   const [searchValue, setSearchValue] = useState('');
 
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const products = await fetchProducts(searchValue);
+
+    setProducts(products);
+    setLoading(false);
+    setSearchValue('');
+  };
   return (
-    <form className="search-bar">
+    <form className="search-bar" onSubmit={handleSearch}>
+      {name}
       <input 
         type="search" 
         value={searchValue}
